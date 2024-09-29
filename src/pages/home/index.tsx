bc86fb9c2b64daff175e5fb540c71e0a315f5983
nohj0518 <nohj0518@daum.net>
2024-09-29 15:36:06 +0900

    template

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -10,11 +10,33 @@ export default function Home() {
           <Link to="/profile"> 프로필</Link>
         </div>
       </header>
-      <div className="post__list"> post list</div>
+      <div className="post__navigation">
+        <div className="post__navigation--active">전체</div>
+        <div>나의 글</div>
+      </div>
+      <div className="post__list">
+        {[...Array(10)].map((e, index) => (
+          <div key={index} className="post__box">
+            <Link to={`/posts/${index}`}>
+              <div className="posts__profile-box">
+                <div className="post__profile" />
+                <div className="post__author-name">현주</div>
+                <div className="post__date">2024.09.29 일요일</div>
+              </div>
+              <div className="posts__title">게시글 {index}</div>
+              <div className="post__text">...</div>
+              <div className="post__utils-box">
+                <div className="post__delete">삭제</div>
+                <div className="post__edit">수정</div>
+              </div>
+            </Link>
+          </div>
+        ))}
+      </div>
       <footer>
-        <div> Menu 1</div>
-        <div> Menu 2</div>
-        <div> Menu 3</div>
+        <Link to="/posts/new"> 글쓰기</Link>
+        <Link to="/posts"> 게시글</Link>
+        <Link to="/profile"> 프로필</Link>
       </footer>
     </div>
   );
