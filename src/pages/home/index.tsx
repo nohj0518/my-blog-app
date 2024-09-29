a49f90ae2d180b6fa973663487374fa59bf1cff0
nohj0518 <nohj0518@daum.net>
2024-09-29 17:00:19 +0900

    remove unused imports

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,6 +1,3 @@
-import { Link } from "react-router-dom";
-import Header from "components/Header";
-import Footer from "components/Footer";
 import PostsPage from "../posts";
 
 export default function Home() {
